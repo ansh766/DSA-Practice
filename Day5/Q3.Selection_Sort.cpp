4d5b7e33bbf288b8ee5c7318b56c72eb8b8322fa
ansh766 <rastogiiansh9@gmail.com>
2026-02-12 21:25:41 +0530

    Create Q3.Selection_Sort.cpp

---
+++ Day5/Q3.Selection_Sort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+         int n = arr.size();
+         for(int i = 0; i<n; i++){
+             int index = i;
+             for(int  j = i+1; j<n; j++){
+                  if(arr[j]<arr[index]){
+                       index = j;
+                  }
+             }
+             swap(arr[i],arr[index]);
+         }
+        
+    }
+};
