4258037f59f0852cc9dc9d04a1009c3f834c48ec
ansh766 <rastogiiansh9@gmail.com>
2026-02-20 18:02:02 +0530

    Implement majority element algorithm in C++

---
+++ Q2.Majority_element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+          int n = nums.size();
+          int count = 0;
+          int element = -1;
+        for(int i= 0; i<n; i++){
+             if(count==0){
+                 element = nums[i];
+                 count = 1;
+             }
+             else{
+                 if(element==nums[i]){
+                     count++;
+                 }
+                 else{
+                     count--;
+                 }
+             }
+        }   
+        return element;   
+    }
+};
